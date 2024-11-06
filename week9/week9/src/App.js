a71997cfe3a87d27290bb9965c882e4442aaf53d
Rosoomin <shtnals020802@naver.com>
2024-11-06 15:51:30 +0900

    rreact

---
+++ week9/week9/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Me from './family/Me';
+import Father from './family/Father';
+import Dog from './family/Dog';
+
+
+ function App() {
+  return (
+  <div className="App">
+    <Me />
+    <Father />
+    <Dog />
+  </div>
+  );
+ }
+ export default App;
